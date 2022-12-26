8b6a97d2d5178d8b47604b1709f487269e2bd6eb
cleitonBarros <48657479+cleitonBarros@users.noreply.github.com>
2022-12-25 23:40:07 -0300

    commit

--- Card/src/components/toggle.jsx
+++ Card/src/components/toggle.jsx
@@ -1,14 +1,14 @@
-function Toggle(){
+// function Toggle(){
   
-    return(
-     <div class="box">
-        <input type="checkbox" name="toggle" class="checkbox" id="toggle"  />
-            <label class="label" for="toggle">
-          <i class="fa-solid fa-sun"></i>
-          <i class="fa-solid fa-moon"></i>
-          <div class="circle"></div>
-        </label>
-      </div>
-    )
-}
-export default Toggle
+//     return(
+//      <div class="box">
+//         <input type="checkbox" name="toggle" class="checkbox" id="toggle"  />
+//             <label class="label" for="toggle">
+//           <i class="fa-solid fa-sun"></i>
+//           <i class="fa-solid fa-moon"></i>
+//           <div class="circle"></div>
+//         </label>
+//       </div>
+//     )
+// }
+// export default Toggle
