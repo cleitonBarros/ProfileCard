5b55ef6c0d6abeeab02c649a21376c7abc2dca3e
cleitonBarros <48657479+cleitonBarros@users.noreply.github.com>
2022-12-28 17:08:07 -0300

    update toggleBtn.jsx
    adicionado localhost

--- Card/src/components/ToggleBtn.jsx
+++ Card/src/components/ToggleBtn.jsx
@@ -1,7 +1,7 @@
 export default function Toggle(props){
     return(
      <div class="box">
-        <input type="checkbox" name="toggle" class="checkbox" id="toggle" onClick={props.value} />
+        <input type="checkbox" name="toggle" checked ={props.chk}class="checkbox" id="toggle" onClick={props.value} />
             <label class="label" for="toggle">
           <i class="fa-solid fa-sun"></i>
           <i class="fa-solid fa-moon"></i>
--- Card/src/pages/Card.jsx
+++ Card/src/pages/Card.jsx
@@ -6,24 +6,30 @@ import  Langswitcher from "../components/Langswitcher"
 
 import Toggle from  '../components/ToggleBtn'
 
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 //import Switch from "react-switch";
 //export  const ThemeContext = createContext(null)
 
 function Card(){
     
-    const [swichToggle , setSwitchToogle] = useState(false)
-    const toggleSwitch = () =>{
-    
-    swichToggle ? setSwitchToogle(false) : setSwitchToogle(true)
-    console.log(swichToggle)
+    const getSwitchToggle = () =>{
+        return JSON.parse(localStorage.getItem('switchToggle')) || false
     }
 
+    const [switchToggle , setSwitchToogle] = useState(getSwitchToggle())
+
+    useEffect(()=>{
+        localStorage.setItem('switchToggle',JSON.stringify(switchToggle))
+    },[switchToggle])
+
+    const toggleSwitch = () =>{
+        switchToggle ? setSwitchToogle(false) : setSwitchToogle(true)
+    }
     return(
 
         <>
-            <div id={swichToggle ? "light": ''} className="container">
-                <Toggle value={toggleSwitch} />
+            <div id={switchToggle ? "light": ''} className="container">
+                <Toggle chk={switchToggle} value={toggleSwitch} />
                 <Information   />
                 <main>
                     <About />
